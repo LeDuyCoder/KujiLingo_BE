6966c4ea56224060373a289613b9152f9dc20072
Kira-Window <duycoder544@gmail.com>
2026-08-21 15:08:54 +0700

    test: ci v0.0.1

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const start = async () => {
             host: "0.0.0.0",
             port: 3000,
         });
-
+        //comment test CI v0.0.1
         console.log("🚀 Server running at http://localhost:3000");
     } catch (err) {
         app.log.error(err);
